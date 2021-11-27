6e34bad5bd08a7a2570086ea3204798504494ced
BOUB <bram.bouwens@witteveenbos.com>
2021-11-27 21:19:21 +0100

    WIP create class LCD_4_5_Digits

---
+++ include/LCD_4_5_Digits.h
@@ -0,0 +1,17 @@
+#ifndef Button_Handler_h
+#define Button_Handler_h
+
+#include <Arduino.h>
+
+// defines
+
+class LCD_4_5_Digits
+{
+private:
+public:
+    LCD_4_5_Digits(/* args */);
+    ~LCD_4_5_Digits();
+    void init(void);
+};
+
+#endif
---
+++ src/LCD-4-5-Digits.cpp
@@ -0,0 +1,15 @@
+
+#include <LCD_4_5_Digits.h>
+
+LCD_4_5_Digits::LCD_4_5_Digits(/* args */)
+{
+}
+
+LCD_4_5_Digits::~LCD_4_5_Digits()
+{
+}
+
+void LCD_4_5_Digits::init(void)
+{
+    int x = 0;
+}
