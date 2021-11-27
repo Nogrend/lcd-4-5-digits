24e003bfe933db466b3942feb05592a2cff0b4ee
BOUB <bram.bouwens@witteveenbos.com>
2021-11-27 23:03:03 +0100

    WIP class LCD_4_5_Digits functionality

--- include/LCD_4_5_Digits.h
+++ include/LCD_4_5_Digits.h
@@ -8,10 +8,33 @@
 class LCD_4_5_Digits
 {
 private:
+    uint8_t _latch_pin;
+    uint8_t _clock_pin;
+    uint8_t _data_pin;
+
+    uint8_t _number[10] = {
+        0b00111111, // "0"
+        0b00000110, // "1"
+        0b01011011, // "2"
+        0b01001111, // "3"
+        0b01100110, // "4"
+        0b01101101, // "5"
+        0b01111101, // "6"
+        0b00000111, // "7"
+        0b01111111, // "8"
+        0b01101111  // "9"
+    };
+
+    uint8_t _value_to_set_lcd[5] = {0x00, 0x00, 0x00, 0x00, 0x00};
+    void _set_lcd(void);
+
 public:
-    LCD_4_5_Digits(/* args */);
+    LCD_4_5_Digits(uint8_t latch, uint8_t clock, uint8_t data);
     ~LCD_4_5_Digits();
     void init(void);
+    void clear(void);
+    void all_on(void);
+    void set_value(uint16_t input_value = 0);
 };
 
 #endif
--- src/LCD_4_5_Digits.cpp
+++ src/LCD_4_5_Digits.cpp
@@ -1,8 +1,11 @@
 
 #include <LCD_4_5_Digits.h>
 
-LCD_4_5_Digits::LCD_4_5_Digits(/* args */)
+LCD_4_5_Digits::LCD_4_5_Digits(uint8_t latch, uint8_t clock, uint8_t data)
 {
+    _latch_pin = latch;
+    _clock_pin = clock;
+    _data_pin = data;
 }
 
 LCD_4_5_Digits::~LCD_4_5_Digits()
@@ -11,5 +14,63 @@ LCD_4_5_Digits::~LCD_4_5_Digits()
 
 void LCD_4_5_Digits::init(void)
 {
-    int x = 0;
+    pinMode(_latch_pin, OUTPUT);
+    pinMode(_clock_pin, OUTPUT);
+    pinMode(_data_pin, OUTPUT);
+    _set_lcd();
+}
+
+void LCD_4_5_Digits::clear(void)
+{
+    for (uint8_t i = 0; i < sizeof(_value_to_set_lcd); i++)
+    {
+        _value_to_set_lcd[i] = 0x00;
+    }
+    _set_lcd();
+}
+
+void LCD_4_5_Digits::all_on(void)
+{
+    for (uint8_t i = 0; i < sizeof(_value_to_set_lcd); i++)
+    {
+        _value_to_set_lcd[i] = 0xff;
+    }
+    _set_lcd();
+}
+
+void LCD_4_5_Digits::set_value(uint16_t input_value)
+{
+    // 1 0 0 0 0
+    //   1 0 0 0
+    //     1 0 0
+    //       1 0
+    //         1
+    uint8_t tth = 0, th = 0, h = 0, t = 0, u = 0; // ten thousands, thousands,hundreds,tens,units
+
+    u = input_value % 10;
+    t = (input_value / 10) % 10;
+    h = (input_value / 100) % 10;
+    th = input_value / 1000;
+
+    _value_to_set_lcd[4] = _number[u];
+    _value_to_set_lcd[3] = _number[t];
+    _value_to_set_lcd[2] = _number[h];
+    _value_to_set_lcd[1] = _number[th];
+
+    if (tth > 0)
+        _value_to_set_lcd[0] |= 0b00010000;
+    else
+        _value_to_set_lcd[0] &= ~0b00010000;
+
+    _set_lcd();
+}
+
+void LCD_4_5_Digits::_set_lcd(void)
+{
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        digitalWrite(_latch_pin, LOW);
+        shiftOut(_data_pin, _clock_pin, MSBFIRST, _value_to_set_lcd[i]);
+        digitalWrite(_latch_pin, HIGH);
+    }
 }
